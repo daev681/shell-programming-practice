38f5b0ff1d5b37351f12d7af97a9b06cc9005800
daev681 <wow1186@naver.com>
2024-08-02 11:41:03 +0000

    반복문 1

---
+++ inflearn/filetest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -e /etc/passwd ]; then
+	echo "exist"
+else
+	echo "not exist"
+fi
+
+
+
---
+++ inflearn/for.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+COLORS="red orange yellow black"
+
+for color in $COLORS
+do
+echo ${color}
+
+done
+
+
+for index in {0..6}
+do
+	echo ${index}
+done
+
+
+
+for ((i = 0; i <= 10; i++))
+do
+	echo ${i}
+done
+
+
+	
